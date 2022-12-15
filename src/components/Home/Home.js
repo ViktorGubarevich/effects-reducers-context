8dfcec6cc11dfddb077aaa041be9f6f923a226f8
Viktor Gubarevich <viktor.gubarevich@zigzag.global>
2022-12-15 17:12:51 +0300

    Added Button in Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,11 @@
+import Button from "../UI/Button/Button";
 import Card from "../UI/Card/Card";
 import styles from "./Home.module.css";
 
-const Home = (props) => (
+const Home = ({ onLogout }) => (
   <Card className={styles.home}>
     <h1>Рады Вас Видеть Снова!</h1>
+    <Button onClick={onLogout}>Выход</Button>
   </Card>
 );
 
